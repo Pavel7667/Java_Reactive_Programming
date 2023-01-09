6ba69e27cd2842884e4320404f921b916de50128
pavel.hnelytsia <pavelgnelicya@gmail.com>
2023-01-09 17:01:39 +0200

    MonoFromFuture Example

---
+++ src/main/java/org/reactor/mono/MonoFromFuture.java
@@ -0,0 +1,22 @@
+package org.reactor.mono;
+
+import org.reactor.utils.Utils;
+import reactor.core.publisher.Mono;
+
+import java.util.concurrent.CompletableFuture;
+
+public class MonoFromFuture {
+
+    public static void main(String[] args) {
+
+        Mono.fromFuture(getName())
+                .subscribe(Utils.onNext());
+
+        Utils.sleepSeconds(1);
+
+    }
+
+    private static CompletableFuture<String> getName() {
+        return CompletableFuture.supplyAsync(() -> Utils.faker().name().fullName());
+    }
+}
