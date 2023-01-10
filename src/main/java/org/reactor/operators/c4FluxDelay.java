22b0db467ddf513915d74f775f1e7d4adaf77174
pavel.hnelytsia <pavelgnelicya@gmail.com>
2023-01-10 20:28:55 +0200

    Flux delay()
    -"reactor.bufferSize.x","8" // 8 as minimum
    - 32 as default

--- src/main/java/org/reactor/operators/c4FluxDelay.java
+++ src/main/java/org/reactor/operators/c4FluxDelay.java
@@ -1,4 +1,22 @@
 package org.reactor.operators;
 
+import org.reactor.utils.Utils;
+import reactor.core.publisher.Flux;
+import reactor.util.concurrent.Queues;
+
+import java.time.Duration;
+
 public class c4FluxDelay {
+
+    public static void main(String[] args) {
+
+        //Queues -> as default as minimum to set 8 - (32)
+        // for max 16 - 256
+        System.setProperty("reactor.bufferSize.x","10");
+
+        Flux.range(1, 100)
+                .log()
+                .delayElements(Duration.ofSeconds(1)) // 32 elements
+                .subscribe(Utils.subscriber());
+    }
 }
