7f0c168006ad8475f47671e674fc6992139c2bed
pavel.hnelytsia <pavelgnelicya@gmail.com>
2023-01-11 18:57:09 +0200

    Overflow Strategy Drop
    -Catching the missing values

--- src/main/java/org/reactor/backpresure/Drop.java
+++ src/main/java/org/reactor/backpresure/Drop.java
@@ -5,12 +5,17 @@ import reactor.core.publisher.Flux;
 import reactor.core.scheduler.Schedulers;
 import reactor.util.concurrent.Queues;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Drop {
 
     public static void main(String[] args) {
 
 //        Queues
         System.setProperty("reactor.bufferSize.small","16");
+
+        List<Object> list = new ArrayList<>();
 
         Flux.create(fluxSink -> {
                     for (int i = 1; i < 501; i++) {
@@ -20,14 +25,17 @@ public class Drop {
                     }
                     fluxSink.complete();
                 })
-                .onBackpressureDrop()
+                .onBackpressureDrop(list::add)
                 .publishOn(Schedulers.boundedElastic())
                 .doOnNext(i -> {
                     Utils.sleepMills(10);
                 })
                 .subscribe(Utils.subscriber());
 
-        Utils.sleepSeconds(60);
+
+        Utils.sleepSeconds(3);
+        System.out.println(list);
+
 
     }
 }
