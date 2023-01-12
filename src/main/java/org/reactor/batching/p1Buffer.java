2376c8c8efd02c44a6f4c45e95353886ecf9df88
pavel.hnelytsia <pavelgnelicya@gmail.com>
2023-01-12 13:02:55 +0200

    Batching .buffer(batch,skip)

--- src/main/java/org/reactor/batching/p1Buffer.java
+++ src/main/java/org/reactor/batching/p1Buffer.java
@@ -11,15 +11,15 @@ public class p1Buffer {
 
 
         eventStream()
-                .bufferTimeout(5,Duration.ofSeconds(1))
+                .buffer(3,1)
                 .doOnNext(e-> System.out.println("list of "+e.size()))
                 .subscribe(Utils.subscriber());
 
-        Utils.sleepSeconds(2); // we're blocking main Thread to see Scheduler work
+        Utils.sleepSeconds(10); // we're blocking main Thread to see Scheduler work
     }
 
     private static Flux<String> eventStream() {
-        return Flux.interval(Duration.ofSeconds(20))
+        return Flux.interval(Duration.ofMillis(300))
                 .map(i -> "event : " + i);
     }
 }
