de432cbf63f8f660bc4d9876802a01044bf5bae5
pavel.hnelytsia <pavelgnelicya@gmail.com>
2023-01-13 14:00:10 +0200

    Batching window() + then() + Duration

--- src/main/java/org/reactor/batching/p2Window.java
+++ src/main/java/org/reactor/batching/p2Window.java
@@ -15,7 +15,7 @@ public class p2Window {
     public static void main(String[] args) {
 
         eventStream()
-                .window(5)
+                .window(Duration.ofSeconds(2))
                 .flatMap(flux->saveEvents(flux))
                 .subscribe(Utils.subscriber());
 
