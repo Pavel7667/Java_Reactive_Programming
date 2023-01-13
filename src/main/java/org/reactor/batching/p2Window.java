28aa42226af044ba5a2bf863bf1359940416ce6d
pavel.hnelytsia <pavelgnelicya@gmail.com>
2023-01-13 13:58:35 +0200

    Batching window() + then()

---
+++ src/main/java/org/reactor/batching/p2Window.java
@@ -0,0 +1,39 @@
+package org.reactor.batching;
+
+import org.reactor.utils.Utils;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
+
+public class p2Window {
+
+    private static AtomicInteger atomicInteger = new AtomicInteger(1);
+
+    public static void main(String[] args) {
+
+        eventStream()
+                .window(5)
+                .flatMap(flux->saveEvents(flux))
+                .subscribe(Utils.subscriber());
+
+        Utils.sleepSeconds(10); // we're blocking main Thread to see Scheduler work
+    }
+
+    private static Flux<String> eventStream() {
+        return Flux.interval(Duration.ofMillis(500))
+                .map(i -> "event : " + i);
+    }
+
+    private static Mono<Integer> saveEvents(Flux<String> flux) {
+        return flux
+                .doOnNext(e -> System.out.println("saving" + e))
+                .doOnComplete(() -> {
+                    System.out.println("saved this batch");
+                    System.out.println("------------");
+                }).then(Mono.just(atomicInteger.getAndIncrement()));
+    }
+}
+
