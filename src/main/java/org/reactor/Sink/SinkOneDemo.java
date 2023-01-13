a8416d38674d78a0494c93fbe151d425ee52df64
pavel.hnelytsia <pavelgnelicya@gmail.com>
2023-01-13 18:40:05 +0200

    Sink.one = Mono

---
+++ src/main/java/org/reactor/Sink/SinkOneDemo.java
@@ -0,0 +1,19 @@
+package org.reactor.Sink;
+
+import org.reactor.utils.Utils;
+import reactor.core.publisher.Mono;
+import reactor.core.publisher.Sinks;
+
+public class SinkOneDemo {
+
+    public static void main(String[] args) {
+
+        // mono 1 value / empty / error
+        Sinks.One<Object> sink = Sinks.one();
+        Mono<Object> mono = sink.asMono();
+        mono.subscribe(Utils.subscriber("sam"));
+
+
+        sink.tryEmitValue(("hi"));
+    }
+}
