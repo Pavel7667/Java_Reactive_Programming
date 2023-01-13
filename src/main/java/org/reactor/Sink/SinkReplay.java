61158e222864ef27972fa435cb3084cb1cb2b90a
pavel.hnelytsia <pavelgnelicya@gmail.com>
2023-01-13 20:03:31 +0200

    Sinks.many().replay().all(10) -cache

--- src/main/java/org/reactor/Sink/SinkReplay.java
+++ src/main/java/org/reactor/Sink/SinkReplay.java
@@ -3,29 +3,23 @@ package org.reactor.Sink;
 import org.reactor.utils.Utils;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Sinks;
-import reactor.util.concurrent.Queues;
 
 import java.time.Duration;
 
-public class SinkMultiDirectAll {
+public class SinkReplay {
     public static void main(String[] args) {
-
         System.setProperty("reactor.bufferSize.small","16");
 
         //  handle through which we would push items
-        Sinks.Many<Object> sink = Sinks.many().multicast().directAllOrNothing();
+        Sinks.Many<Object> sink = Sinks.many().replay().all(10);
 
         //  handle through which subscribers will receive items
         Flux<Object> flux = sink.asFlux();
 
+        sink.tryEmitNext("hi");
+        sink.tryEmitNext("how are you   ?");
         flux.subscribe(Utils.subscriber("sam"));
-        flux.delayElements(Duration.ofMillis(200)).subscribe(Utils.subscriber("mike"));
-
-        for (int i = 0; i < 100; i++) {
-            sink.tryEmitNext(i);
-        }
-
-        Utils.sleepSeconds(10);
-
+        flux.subscribe(Utils.subscriber("mike")); // will go
+        sink.tryEmitNext("???");
     }
 }
